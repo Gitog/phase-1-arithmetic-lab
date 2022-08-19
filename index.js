618d0edb122537dc9cb9e060d206e8d11997e32c
James <jamesnjorio@gmail.com>
2022-08-19 23:59:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 
 // Write your code here
+// function multiply(num1, num2) {
+
+//     return num1 * num2;
+// }
+// multiply(2, 31)
+
+// function random() {
+//     const rInteger = Math.floor(Math.random() * 10) + 1
+//     return rInteger;
+// }
+// random();
+
+// function mod(num3, num4) {
+//     return num3 / num4;
+// }
+// mod(100 / 25)
+
+// function max(mySet) {
+
+//     return Math.max(...mySet);
+
+// }
+// max([1, 2, 3, 4, 20])
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 104;
+const num4 = 25;
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 3, 4, 5, 7, 8, 20)
